51896f81eaf983cd24e6b6a0b827d06d9309d977
Devendra Ghanekar <devaghanekar23@gmail.com>
2026-08-07 11:39:53 +0530

    new project

---
+++ apache-monitor.sh
@@ -0,0 +1,5 @@
+Apache2 Status
+ #sudo systemctl status apache2
+
+Restart Apache2
+ #sudo systemctl restart apache2        
---
+++ health-check.sh
@@ -0,0 +1,17 @@
+CPU Usage 
+ # top
+
+Memory check
+ # free -h
+
+Disk check
+ df -h
+
+Users check
+ # who
+
+Processes check
+ # ps aux
+
+Uptime process runing
+ # uptime
